81f37352af635c95cbbde2797d9cb602cac0f623
Kanishak <i.kanishakm@gmail.com>
2025-02-16 22:45:37 +0530

    changess

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Scene } from "./Scene"
 function App() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
-      <Canvas style={{ background: "#000" }} camera={{ position: [0, 0, 1] }} >
+      <Canvas gl={{ alpha: true }} camera={{ position: [0, 0, 1] }} >
         <Scene />
       </Canvas>
     </div>
