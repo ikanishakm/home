5f153fe266a93b77c1cabab50265722bb7040e12
Kanishak <i.kanishakm@gmail.com>
2025-02-16 22:35:09 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import './App.css'
-import { Canvas } from '@react-three/fiber'
-import { Scene } from './Scene'
+import "./App.css"
+import { Canvas } from "@react-three/fiber"
+import { Scene } from "./Scene"
 
 function App() {
-
   return (
-    <div style={{ width: '100%', height: '100vh'}}>
-      <Canvas>
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Canvas style={{ background: "#000" }} camera={{ position: [0, 0, 1] }} >
         <Scene />
       </Canvas>
-    </div >
+    </div>
   )
 }
 
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -43,7 +43,6 @@ export function Scene() {
 
   return (
     <>
-      <OrbitControls />
       <CylinderGroup />
       {/* <group>
         {cylinderPos.map((pos, index) => (
